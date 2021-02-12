49aad4eee1660e87fcfe17b22225fdf4db260bec
KEC79 <mrsshoes2@gmail.com>
2021-02-12 23:33:48 +0000

    Add prettier formatting

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-import React from 'react';
-import './App.css';
-import { TodoForm } from './components/TodoForm';
-import { Todo } from './components/Todo'
+import React from "react"
+import "./App.css"
+import { TodoForm } from "./components/TodoForm"
+import { Todo } from "./components/Todo"
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export const App = () => {
-  const [todos, setTodos] = React.useState([
-    { text: "Game 1", isCompleted: false },
-    { text: "Game 2", isCompleted: false },
-    { text: "Game 3", isCompleted: false }
-  ]);
+    const [todos, setTodos] = React.useState([
+        { text: "Game 1", isCompleted: false },
+        { text: "Game 2", isCompleted: false },
+        { text: "Game 3", isCompleted: false },
+    ])
 
-  const addTodo = text => {
-    const newTodos = [...todos, { text }];
-    setTodos(newTodos);
-  };
+    const addTodo = (text) => {
+        const newTodos = [...todos, { text }]
+        setTodos(newTodos)
+    }
 
-  const completeTodo = index => {
-    const newTodos = [...todos];
-    newTodos[index].isCompleted = true;
-    setTodos(newTodos);
-  };
+    const completeTodo = (index) => {
+        const newTodos = [...todos]
+        newTodos[index].isCompleted = true
+        setTodos(newTodos)
+    }
 
-  const removeTodo = index => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
+    const removeTodo = (index) => {
+        const newTodos = [...todos]
+        newTodos.splice(index, 1)
+        setTodos(newTodos)
+    }
 
     return (
-      <div className="app">
-        <h2>Game To Do List</h2>
-        <div className="todo-list">
-          {todos.map((todo, index) => (
-            <Todo
-              index={index}
-              todo={todo}
-              completeTodo={completeTodo}
-              removeTodo={removeTodo}
-            />
-          ))}
-          <TodoForm addTodo={addTodo} />
+        <div className="app">
+            <h2>Game To Do List</h2>
+            <div className="todo-list">
+                {todos.map((todo, index) => (
+                    <Todo
+                        index={index}
+                        todo={todo}
+                        completeTodo={completeTodo}
+                        removeTodo={removeTodo}
+                    />
+                ))}
+                <TodoForm addTodo={addTodo} />
+            </div>
         </div>
-      </div>
-    );
+    )
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,21 @@
-import { CgRemove } from 'react-icons/cg'
-import Button from 'react-bootstrap/Button';
+import { CgRemove } from "react-icons/cg"
+import Button from "react-bootstrap/Button"
 
 export const Todo = ({ todo, index, completeTodo, removeTodo }) => {
     return (
-      <div className="todo"
-      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
-        {todo.text}
-        <div>
-          <Button variant="info" onClick={() => completeTodo(index)}>Complete</Button>{' '}
-          <Button variant="info" onClick={() => removeTodo(index)}><CgRemove/></Button>{' '}
+        <div
+            className="todo"
+            style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+        >
+            {todo.text}
+            <div>
+                <Button variant="info" onClick={() => completeTodo(index)}>
+                    Complete
+                </Button>{" "}
+                <Button variant="info" onClick={() => removeTodo(index)}>
+                    <CgRemove />
+                </Button>{" "}
+            </div>
         </div>
-      </div>
-    );
-  };
+    )
+}
