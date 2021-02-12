19b3668afb45328ac4b2a37c9940c4b5ead1ab81
KEC79 <mrsshoes2@gmail.com>
2021-02-12 22:42:46 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { TodoForm } from './components/TodoForm';
+import { Todo } from './components/Todo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const App = () => {
+  const [todos, setTodos] = React.useState([
+    { text: "Game 1", isCompleted: false },
+    { text: "Game 2", isCompleted: false },
+    { text: "Game 3", isCompleted: false }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+    return (
+      <div className="app">
+        <h2>Game To Do List</h2>
+        <div className="todo-list">
+          {todos.map((todo, index) => (
+            <Todo
+              index={index}
+              todo={todo}
+              completeTodo={completeTodo}
+              removeTodo={removeTodo}
+            />
+          ))}
+          <TodoForm addTodo={addTodo} />
+        </div>
+      </div>
+    );
 }
-
-export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import { CgRemove } from 'react-icons/cg'
+import Button from 'react-bootstrap/Button';
+
+export const Todo = ({ todo, index, completeTodo, removeTodo }) => {
+    return (
+      <div className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+        {todo.text}
+        <div>
+          <Button variant="info" onClick={() => completeTodo(index)}>Complete</Button>{' '}
+          <Button variant="info" onClick={() => removeTodo(index)}><CgRemove/></Button>{' '}
+        </div>
+      </div>
+    );
+  };
