6c26e78ccc7769a5e4f92b036a7c906af12da78d
KEC79 <mrsshoes2@gmail.com>
2021-02-13 01:29:11 +0000

    Added card and title

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import { TodoForm } from "./components/TodoForm";
 import { Todo } from "./components/Todo";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Card from "react-bootstrap/Card";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -31,18 +33,32 @@ export const App = () => {
 
     return (
         <div className="app">
-            <h2>Game To Do List</h2>
+            <Jumbotron>
+                <h1>Game Lists</h1>
+                <p>
+                    This is a place where you can list games that you want to
+                    play and cross them off when completed.
+                </p>
+            </Jumbotron>
             <div className="todo-list">
-                {todos.map((todo, index) => (
-                    <Todo
-                        key={index}
-                        index={index}
-                        todo={todo}
-                        completeTodo={completeTodo}
-                        removeTodo={removeTodo}
-                    />
-                ))}
-                <TodoForm addTodo={addTodo} />
+                <Card style={{ width: "390px" }}>
+                    <Card.Body>
+                        <Card.Title>Your list of games</Card.Title>
+                        <Card.Text>
+                            Add all the games you want to play here.
+                        </Card.Text>
+                        {todos.map((todo, index) => (
+                            <Todo
+                                key={index}
+                                index={index}
+                                todo={todo}
+                                completeTodo={completeTodo}
+                                removeTodo={removeTodo}
+                            />
+                        ))}
+                        <TodoForm addTodo={addTodo} />
+                    </Card.Body>
+                </Card>
             </div>
         </div>
     );
